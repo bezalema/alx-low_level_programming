7155f63dfc911143b26985e5cd187a35d748f82f
bezalema <bezawitledea@gmail.com>
2022-06-30 14:53:29 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints statment
+ * return: always 0 (success)
+ * return: 1 if else
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
