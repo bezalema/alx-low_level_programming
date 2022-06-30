8a354ef12ac40e3481155d540703e157572bf5b0
bezalema <bezawitledea@gmail.com>
2022-06-30 13:51:21 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * return: always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
